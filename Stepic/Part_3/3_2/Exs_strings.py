ac1b0f2c62d5936b8f798439da39cdf0de024f33
YoTM <yolkin_t@list.ru>
2018-01-15 18:03:10 +0300

    Реорганизация заданий по частям курса

---
+++ Stepic/Part_3/3_2/Exs_strings.py
@@ -0,0 +1,22 @@
+# print("abc" in "abcba")
+# print("abce" in "abcba")
+#
+# print("cabcd".find("abc") # индекс первого вхождения или -1
+# # print(str.find.__doc__)
+# print("cabcd"[1:].find("abc"))
+
+# print("cabcd".index("abc")) # Индекс первого вхождения или ValueError
+# print("cabcd".index("aec"))
+
+# s = "The man in black fled across the desert, and the gunslinger followed"
+# print(s.startswith("The man in black"))
+# print(s.startswith(("The man in black", "The dog", "The woman")))
+# print(s.startswith.__doc__)
+
+# s = "image.png"
+# print(s.endswith(".png"))
+
+# s = "ababa"
+# print(s.count("aba")) # Кол-во вхождений подстроки БЕЗ пересечения
+# print(s.count.__doc__)
+
