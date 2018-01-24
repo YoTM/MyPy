7e95cb02bc3f7d2d75c66b38671dac18ea21e1e9
YoTM <yolkin_t@list.ru>
2018-01-24 09:18:29 +0300

    Решены задачи 1-2 к уроку 3 части 3

--- Stepic/Part_3/3_3 - Regular/3_3 - Z-1.py
+++ Stepic/Part_3/3_3 - Regular/3_3 - Z-1.py
@@ -1,9 +1,17 @@
 import sys
 import re
 
-pattern = r"(cat).{2,}"
+pattern = r"(cat.*){2,}"
+# pattern = r"(cat).*\1"
 
 for line in sys.stdin:
     line = line.rstrip()
-    match = re.match(pattern, line)
-    print(match)
+    match = re.search(pattern, line)
+    if match:
+        print(line)
+
+# Преподаватель:
+# for line in sys.stdin:
+#     line = line.strip()
+#     if re.search(r"cat.*cat", line):
+#         print(line)
