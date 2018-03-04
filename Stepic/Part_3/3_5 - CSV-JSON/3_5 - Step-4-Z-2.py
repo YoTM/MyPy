df5c05fceb27e3e186eff669a70fcb1018586d34
YoTM <yolkin_t@list.ru>
2018-03-04 23:08:47 +0300

    Решил последнюю задачу по подсчёту предков в дереве классов. Входные данные в JSON

--- Stepic/Part_3/3_5 - CSV-JSON/3_5 - Step-4-Z-2.py
+++ Stepic/Part_3/3_5 - CSV-JSON/3_5 - Step-4-Z-2.py
@@ -35,6 +35,20 @@ def is_parent(child, parent):
     """
     return child == parent or any(map(lambda p: is_parent(p, parent), parents[child]))
 
+def get_classes(data_dict):
+    """
+        Ф-я создаёт набор классов из словаря. Классы не повторяются
+
+    """
+    class_set = set()
+    for s in data_dict.keys():
+        class_set.add(s)
+        if data_dict[s] != []:
+            for c in data_dict[s]:
+                class_set.add(c)
+    return class_set
+
+
 # Получаем входные данные в формате JSON
 data = json.loads(input())
 
@@ -48,13 +62,49 @@ for d in data:
     # Наполняем словарь данными вида класс : предки
     parents[d["name"]] = d["parents"]
 
-for x in parents.keys():
-    for y in parents.keys():
-        if is_parent(x, y):
-            if x in res_dict:
-                res_dict[x] += 1
+# Создаем мн-во/набор всех классов
+all_classes = get_classes(parents)
+
+for current_class in all_classes:
+    for c in all_classes:
+        if is_parent(c, current_class):
+            if current_class in res_dict:
+                res_dict[current_class] += 1
             else:
-                res_dict[x] = 1
+                res_dict[current_class] = 1
+
+for k in sorted(res_dict.keys()):
+    print (k, ':', res_dict[k])
 
 
-print(res_dict)
+# Решение препода:
+# import json
+#
+# data = json.loads(input())
+# children = dict()
+#
+# for cls in data:
+#     for par in cls["parents"]:
+#         if par not in children:
+#             children[par] = []
+#         children[par].append(cls["name"])
+#
+# def dfs(v, used):
+#     size = 1
+#     used.add(v)
+#     if v not in children:
+#         return size
+#
+#     for child in children[v]:
+#         if child not in used:
+#             size += dfs(child, used)
+#
+#     return size
+#
+# ans = []
+#
+# for cls in data:
+#     ans.append((cls["name"], dfs(cls["name"], set())))
+#
+# for i in sorted(ans):
+#     print(i[0], ":", i[1])
