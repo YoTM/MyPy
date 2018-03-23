f70cfbdeb46f4c02612d3af9bfc231f12ee79e52
YoTM <yolkin_t@list.ru>
2018-03-24 00:03:40 +0300

    Первая версия решения (нерабочая) задачи про кубики в XML

--- Stepic/Part_3/3_7 - XML/3_7 - Exs_1-XML.py
+++ Stepic/Part_3/3_7 - XML/3_7 - Exs_1-XML.py
@@ -1,11 +1,39 @@
 from xml.etree import ElementTree as ET
-from lxml import etree
+
+def get_child(root, scale):
+    """
+        Ф-я получает "ветку" XML-дерева и вес уровня - перебирает все уровни рекурсивно
+    :param root:
+    :return:
+    """
+    # Пока есть подуровни
+    for child in root:
+        # Увеличиваем ценность уровня
+        scale += 1
+        # Рекурсивно погружаемся дальше
+        get_child(child, scale)
+        # Возвращаемся и записываем ценность цвета к общему счету в словаре
+        color_scale[child.get('color')] += scale
+
+        # print('check-1')
 
 # tree = ElementTree.parsestring(input())
 data = input()
 root = ET.fromstring(data)
 
+# Словарь ценности цветов: Красного, Зеленого и Синего
+color_scale = {'red': 0,
+               'green': 0,
+               'blue': 0
+               }
 
-print(root)
-for child in root:
-    print(child.tag, child.attrib)
+
+start_scale = 1
+get_child(root, start_scale)
+
+print(color_scale)
+
+
+# print(root)
+# for child in root:
+#     print(child.tag, child.attrib)
